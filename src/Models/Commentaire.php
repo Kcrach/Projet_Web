0dc9b6c0d02bb0ed07e713366a066c757cd1604e
Xx-louloudu71-xX <louisguillotin.lg@gmail.com>
2018-01-15 13:49:45 +0100

    dd
    dd

--- src/Models/Commentaire.php
+++ src/Models/Commentaire.php
@@ -4,11 +4,14 @@ namespace pw\Models;
 
 class Commentaire
 {
+	protected $idArticle;
 	protected $id;
 	protected $contenu;
 
-	public function __construct($contenu){
+	public function __construct($contenu,$id,$idArticle){
 		$this->contenu = $contenu;
+		$this->id=$id;
+		$this->idArticle = $idArticle;
 	}
 
 	public function getId(){
@@ -17,6 +20,10 @@ class Commentaire
 
 	public function getContenu(){
 		return $this->contenu;
+	}
+
+	public function getIdArticle(){
+		return $this->idArticle;
 	}
 }
 
